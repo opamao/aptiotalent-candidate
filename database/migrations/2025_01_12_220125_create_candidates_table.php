8169ea2055f465fb5532daef182d95571183d2d2
Theodore YAPI <kadhelaman@192.168.1.7>
2025-01-14 00:00:09 +0000

    suite create table

--- database/migrations/2025_01_12_220125_create_candidates_table.php
+++ database/migrations/2025_01_12_220125_create_candidates_table.php
@@ -12,7 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('candidates', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('idcandidat')->primary();
+            $table->string('photo_cand')->nullable();
+            $table->string('sexe_cand')->comment('homme, femme, autres');
+            $table->string('nom_cand');
+            $table->string('prenom_cand');
+            $table->string('email_cand')->unique();
+            $table->string('phone_cand', 50)->unique();
+            $table->string('habitation_cand')->nullable();
+            $table->string('experience_an_cand')->nullable();
+            $table->string('experience_mois_cand')->nullable();
+            $table->string('salaire_cand')->nullable();
+            $table->string('password_cand');
+            $table->integer('status_cand')->default(1)->comment('1 = active, 2 = inactive');
             $table->timestamps();
         });
     }
